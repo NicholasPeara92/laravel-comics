750923f965bcd88feb59d27ce80705d64e6666f6
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-13 17:34:21 +0100

    Add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,75 @@
-
+<footer>
+    <div class="bg">
+        <div class="container">
+            <div class="col-6 d-flex ">
+                <div class="col">
+                    <ul>
+                        <li>
+                            <h3>DC COMICS</h3>
+                        </li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <h3>SHOP</h3>
+                        </li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul>
+                        <li>
+                            <h3>DC</h3>
+                        </li>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy Policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul>
+                        <li>
+                            <h3>SITES</h3>
+                        </li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+            <p>All Site Content TM and <i class="fa-regular fa-copyright"></i> 2020 DC Entertainment, unless
+                oterwise <a href="#">noted here</a>. All rights reserved. <br><a href="#">Cokies
+                    Settings</a></p>
+        </div>
+    </div>
+    <div id="copyright" class="container">
+        <button><a href="#">SIGN-UP NOW</a></button>
+        <div class="follow">
+            <ul>
+                <li>FOLLOW US</li>
+                <li><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-periscope.pn') }}" alt=""></li>
+            </ul>
+        </div>
+    </div>
+</footer>
