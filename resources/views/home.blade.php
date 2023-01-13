48299d77884ca99ea4ecb04a7c592c5399f39ac1
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-13 16:05:03 +0100

    Add comic list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.main')
 
 @section('page-title')
-    Homepage
+    Home
 @endsection
 
 @section('page-content')
+    <div class="container pt-3">
+        @foreach ($comics as $comic)
+            <div class="ms-card">
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}" />
+                <div class="title">{{ $comic['series'] }}</div>
+            </div>
+        @endforeach
+    </div>
+    <button>LOAD MORE</button>
+    </main>
+    {{-- <AppPrefooter /> --}}
 @endsection
