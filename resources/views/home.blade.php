c5301cb847e065981838169589c23761cca938a6
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-13 16:49:23 +0100

    Add prefooter

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,31 @@
         @endforeach
     </div>
     <button>LOAD MORE</button>
+    <section class="pre-footer">
+        <div class="container">
+            <ul>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <a href="#">DIGITAL COMICS</a>
+                </li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <a href="#">DC MERCHANDISE</a>
+                </li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <a href="#">SUBSCRIPTION</a>
+                </li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <a href="#">COMIC SHOP LOCATOR</a>
+                </li>
+                <li>
+                    <img class="mt-3" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <a href="#">DC POWER VISA</a>
+                </li>
+            </ul>
+        </div>
+    </section>
     </main>
-    {{-- <AppPrefooter /> --}}
 @endsection
